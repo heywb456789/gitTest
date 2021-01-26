229b3f05d23a83ea3398975e6f58bf3f8b1f2132
김우빈 <heywb457@tricycle.co.kr>
2021-01-26 13:04:00 +0900

    first Commit

---
+++ src/main/java/com/gitTest/src/ApiTest.java
@@ -0,0 +1,15 @@
+package com.gitTest.src;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiTest {
+    
+    @GetMapping
+    @RequestMapping("/hi")
+    public String getMethod(){
+        return "hi";
+    }
+}
